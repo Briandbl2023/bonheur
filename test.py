ae5fc3d1b250945f918be41694cc3df4326c2c71
Briandbl2023 <147381656+Briandbl2023@users.noreply.github.com>
2023-10-09 15:28:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import numpy as np
 import sklearn
 from sklearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.preprocessing import TargetEncoder, OneHotEncoder
