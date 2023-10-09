ce0600c7886b15f28bcd5c92275b80eb95497b2e
Briandbl2023 <147381656+Briandbl2023@users.noreply.github.com>
2023-10-09 12:58:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -35,7 +35,6 @@ if option == 'Présentation':
     st.write("C'est la page de présentation du projet.")
         
     st.write(df4.head())  # Affiche les premières lignes du DataFrame
-    st.write(df4.isna().sum())
 
 elif option == 'Quelques visualisations':
     st.header("Quelques visualisations du projet")
