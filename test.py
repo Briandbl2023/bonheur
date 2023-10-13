665d0e2c523937f2606cd3a0a5ef4a31b6feedaa
Briandbl2023 <147381656+Briandbl2023@users.noreply.github.com>
2023-10-13 15:39:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -448,7 +448,7 @@ elif option == "Prédictions":
                     y_preds = model.predict(X_tests)
                     y_pred_saisie = model.predict(X_train_new)
                     st.write(y_pred_saisie)
-                    #st.header("Prédiction : ",y_pred_saisie[0])
+                    #st.header(y_pred_saisie[0])
             
                 elif model_name =='KNN':
                     model.fit(X_traink, y_traink)
