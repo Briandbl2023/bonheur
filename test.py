5e5c725d4846147b23a9f1b443e745480597f211
Briandbl2023 <147381656+Briandbl2023@users.noreply.github.com>
2023-10-22 09:37:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -815,8 +815,17 @@ elif option == "Prédictions":
     if submit_button:
         selected_model = st.session_state.model_select
         # Filtrer les valeurs non vides
-        user_inputs = {key: value for key, value in user_inputs.items() if value != ''}
-        user_inputs = {clef: float(valeur.replace(',', '.')) for clef, valeur in user_inputs.items()}
+#        user_inputs = {key: value for key, value in user_inputs.items() if value != ''}
+#        user_inputs = {clef: float(valeur.replace(',', '.')) for clef, valeur in user_inputs.items()}
+        # Dictionnaire pour stocker les valeurs saisies par l'utilisateur
+        user_inputs = {}
+
+        # Zones de texte pour les colonnes du DataFrame df_ensemble à partir de la deuxième colonne
+        for column in list(df_ensemble.columns)[2:-1]:
+          user_inputs[column] = st.text_input(column)
+
+        # Remplacer les champs vides par np.nan
+        user_inputs = {key: float(value.replace(',', '.')) if value != '' else np.nan for key, value in user_inputs.items()}
 
         # Créer un DataFrame à partir du dictionnaire
         X_new = pd.DataFrame([user_inputs])
