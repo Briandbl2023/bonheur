98cf5d89bc3e5da6e848cf19b9381a56e09ff770
Briandbl2023 <147381656+Briandbl2023@users.noreply.github.com>
2023-10-23 14:00:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -771,7 +771,7 @@ elif option == 'Modélisation':
                 st.write(pourcentage_bonnes_reponses)
                 st.write("<b><u>Features importance</u></b>",unsafe_allow_html=True)
                 st.write("<ul><li>Les indicateurs socio économiques sont privilégiés</li><li>Seul l'indicateur Healthy a été relayé au second plan</li><li>Les variables Generosity et Perceptions of corruption étaient les moins corrélées (en accord avec notre exloration)</li></ul>",unsafe_allow_html=True)
-                st.write(f"<table><tr><th>Target Encoder</th><th>MAE</th><th>RMSE</th></tr><tr><td>Jeu d'entrainement</td><td>{mae}</td><td>{rmse}</td></tr></table>",unsafe_allow_html=True)
+                st.write(f"<table><tr><th>Target Encoder</th><th>MAE</th><th>RMSE</th></tr><tr><td>Jeu d'entrainement</td><td>{mae}</td><td>{rmse}</td></tr><tr><td>Prédictions 2021</td><td>{mae2}</td><td>{rmse2}</td></tr></table>",unsafe_allow_html=True)
             elif model_name =='KNN':
                 st.image(pknn)
                 st.write("<b><u>Optimisation</b></u>",unsafe_allow_html=True)
